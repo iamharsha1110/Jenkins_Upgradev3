738927198753fe8cca09228cc575cb102d594eb8
I S S Harsha <137803974+iamharsha1110@users.noreply.github.com>
2025-01-21 11:07:24 +0530

    removing test cases TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,11 +27,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
-
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
